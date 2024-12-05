d6d1e871863dabba4604fd6024050eb06f6a54ef
高木慶治 <takagiyoshiharu@takagikeijinoMacBook-Air.local>
2024-12-05 16:14:35 +0900

    12 5

---
+++ ft_putadress.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putadress.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: takagiyoshiharu <takagiyoshiharu@studen    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/05 13:51:21 by takagiyoshi       #+#    #+#             */
+/*   Updated: 2024/12/05 13:51:32 by takagiyoshi      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_putadress(unsigned long nb)
+{
+	int		re;
+	char	*base;
+
+	re = 0;
+	base = "0123456789abcdef";
+	if (nb >= 16)
+	{
+		re += ft_putadress(nb / 16);
+		re += ft_putadress(nb % 16);
+	}
+	else
+		re += ft_putchar(base[nb % 16]);
+	return (re);
+}
